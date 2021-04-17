44413891538b75dc5c4c912ac442e449e77cd319
Ajitzi Carmona <ajitzi.carmona@outlook.com>
2021-04-17 15:32:16 +0200

    ajout sous-menu et corrections

--- script/menu_burger.js
+++ script/menu_burger.js
@@ -1,8 +1,17 @@
 const toggleMenu = document.querySelector('.toggle-menu');
 const menu = document.querySelector('.main-nav');
+const menuIcon = document.querySelector('.menu-icon');
 
 toggleMenu.addEventListener('click', function() {
     const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
     toggleMenu.setAttribute('aria-expanded', !open);
     menu.hidden = !menu.hidden;
+});
+
+toggleMenu.addEventListener('click', function() {
+    if (menuIcon.classList.contains("is-closed")) {
+        menuIcon.classList.replace("is-closed", "is-opened")
+    } else if (menuIcon.classList.contains("is-opened")) {
+        menuIcon.classList.replace("is-opened", "is-closed")
+    }
 });
